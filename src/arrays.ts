49317a836157fab029cb606559fc6a1569e13f4e
aaron oster <bigaaron@udel.edu>
2023-02-22 11:31:13 -0500

    solved strings to INts

--- src/arrays.ts
+++ src/arrays.ts
@@ -30,7 +30,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const Integers = numbers.map((num: string): number =>
+        isNaN(Number(num)) ? 0 : Number(num)
+    );
+    return Integers;
 }
 
 /**
