7449a5db14e39549ecac1ab7dbe0c0a69dc66233
aaron oster <bigaaron@udel.edu>
2023-03-10 15:28:13 -0500

    commented out errors that were causing lint errors

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,7 +1,7 @@
-import { spawn } from "child_process";
+// import { spawn } from "child_process";
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { text } from "stream/consumers";
+// import { text } from "stream/consumers";
 
 /**
  * Here is a helper function you *must* use to "roll" your die.
